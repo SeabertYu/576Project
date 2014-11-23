c48a71b8411f3b897d0e25486bce63a165372f7b
SeabertYu <seabert.yu@gmail.com>
2014-11-22 23:50:07 -0800

    JPG and Entropy

--- ImageParser.py
+++ ImageParser.py
@@ -1,26 +1,38 @@
 __author__ = 'Boyang'
 from math import *
+from os import listdir
 
 HEIGHT = 288
 WIDTH = 352
-LENGTH = HEIGHT*WIDTH
-file = open('./dataset/image001.rgb', 'rb')
-buffer = file.read()
-offset = 0
-histogram = [[0 for i in range(256)] for j in range(3)];
+LENGTH = HEIGHT * WIDTH
+FOLDER = "./dataset/"
 
 
-for i in range(HEIGHT):
-    for j in range(WIDTH):
-        for k in range(3):
-            b = buffer[offset+LENGTH*k]
-            histogram[k][b&0xff]+=1
-    offset += 1
+def printEntropy():
+    counter = 1
+    for file in listdir(FOLDER):
+        print(counter, " : ", getEntropy(file))
+        counter += 1
 
-entropy = 0.0
-for k in range(3):
-    for i in range(256):
-        p = float(histogram[k][i])/LENGTH
-        if p!= 0:
-            entropy-=p*log(p,2)
-print(entropy)
+
+def getEntropy(file):
+    file = open(FOLDER + file, 'rb')
+    buffer = file.read()
+    offset = 0
+    histogram = [[0 for i in range(256)] for j in range(3)]
+    for i in range(HEIGHT):
+        for j in range(WIDTH):
+            for k in range(3):
+                b = buffer[offset + LENGTH * k]
+                histogram[k][b & 0xff] += 1
+        offset += 1
+    entropy = 0.0
+    for k in range(3):
+        for i in range(256):
+            p = float(histogram[k][i]) / (LENGTH*3)
+            if p != 0:
+                entropy -= p * log(p, 2)
+    return entropy
+
+
+printEntropy()
